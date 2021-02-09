c5a00ad276ec62039c8d1fad7c7cba038c335316
MarcusJaxon <Marcus.jaxon@sainsburys.co.uk>
2021-02-09 16:37:28 +0000

    added a new test

--- test_app.py
+++ test_app.py
@@ -1,29 +1,22 @@
 import os
-from modules.utils import Last_Num
+import unittest
+import modules.utils
 
-# test to see if it gets the last customer id, and adds 1 to it
-def test_Last_Number():
-    #assemble
-    test_list = [
-    {"customer_id" : 1,"customer_name" : "Marcus","status" : "Accepted"},
-    {"customer_id" : 2,"customer_name" : "Steve","status" : "Accepted"},
-    {"customer_id" : 3,"customer_name" : "John","status" : "Accepted"}]
-    expected = "4"
-    #act
-    actual = Last_Num(test_list)
-    # Assert
-    assert expected == actual
-    print("Passed test 1")
-test_Last_Number()
-    
-# #Test to see what happens if an empty table is passed to the function
-def TestEmptyTablePassed():
-    #assemble
-    emptytable = []
-    expected = "1"
-    #act
-    actual = Last_Num(emptytable)
-    # Assert
-    assert expected == actual
-    print("Passed test 2")
-TestEmptyTablePassed()
+class TestUtils(unittest.TestCase):
+    def test_Last_Number(self):
+        test_list = [
+        {"customer_id" : 1,"customer_name" : "Marcus","status" : "Accepted"},
+        {"customer_id" : 2,"customer_name" : "Steve","status" : "Accepted"},
+        {"customer_id" : 3,"customer_name" : "John","status" : "Accepted"}]
+        # testing Last_Num gives the correct next number. 4
+        result = modules.utils.Last_Num(test_list)
+        self.assertEqual(result, "4") #(result, expected): assertEquals checks if result equals expected 
+        self.assertEqual(modules.utils.Last_Num([]), "1") # test what happens when passing an emppty list
+
+
+class TestDataPersistance(unittest.TestCase):
+    def test_FromCSV(self):
+        print("")
+        
+if __name__ == "__main__":
+    unittest.main()
