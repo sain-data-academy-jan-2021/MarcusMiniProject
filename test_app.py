11588d857f20b6634fbb6de6ddaa7118c427b8f9
MarcusJaxon <Marcus.jaxon@sainsburys.co.uk>
2021-02-23 09:43:19 +0000

    Setting up new test

--- test_app.py
+++ test_app.py
@@ -1,22 +1,13 @@
 import os
 import unittest
+from unittest.mock import patch
 import modules.utils
 
-class TestUtils(unittest.TestCase):
-    def test_Last_Number(self):
-        test_list = [
-        {"customer_id" : 1,"customer_name" : "Marcus","status" : "Accepted"},
-        {"customer_id" : 2,"customer_name" : "Steve","status" : "Accepted"},
-        {"customer_id" : 3,"customer_name" : "John","status" : "Accepted"}]
-        # testing Last_Num gives the correct next number. 4
-        result = modules.utils.Last_Num(test_list)
-        self.assertEqual(result, "4") #(result, expected): assertEquals checks if result equals expected 
-        self.assertEqual(modules.utils.Last_Num([]), "1") # test what happens when passing an emppty list
+class TestMenus(unittest.TestCase):
+    def test_main_menu_choice(self):
+        
+        
 
-
-class TestDataPersistance(unittest.TestCase):
-    def test_FromCSV(self):
-        print("")
         
 if __name__ == "__main__":
     unittest.main()
