d43a560a1e1b4d1fc0c6b90df65a254dd657a2e3
MarcusJaxon <Marcus.jaxon@sainsburys.co.uk>
2021-03-15 09:48:14 +0000

    daily git kata

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from modules.menus import MainMenu
-
+# Never gonna give you up
+# Never gonna let you down
+# Never gonna run around and desert you
+# Never gonna make you cry
+# Never gonna say goodbye
+# Never gonna tell a lie and hurt you
 MainMenu()
